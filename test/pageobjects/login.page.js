f0465ff26fed8170a9626af0d95a1540ee1227c0
Léo Kyrillos <leokyrillos@gmail.com>
2024-03-04 23:30:57 -0500

    first commit

---
+++ test/pageobjects/login.page.js
@@ -0,0 +1,26 @@
+import { $ } from '@wdio/globals'
+
+
+class LoginPage {
+    
+    //get por ID
+    get email (){
+        return $('id:email')
+    }
+    //get por texto
+    get password (){
+        return $('android=new UiSelector().text("Password")')
+    }
+    //get assebility ID
+    get btnLogin (){
+        return $('~Login')
+    }
+
+    async login(email, password){
+        await this.email.setValue(email)
+        await this.password.setValue(password)
+        await this.btnLogin.click()
+    }   
+}
+
+export default new LoginPage();
---
+++ test/specs/login.test.js
@@ -0,0 +1,14 @@
+import { expect, driver } from '@wdio/globals'
+import homePage from '../pageobjects/home.page'
+import loginPage from '../pageobjects/login.page'
+import profilePage from '../pageobjects/profile.page'
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await homePage.openMenu('profile')
+        await loginPage.login('cliente@ebac.art.br', 'GD*peToHNJ1#c$sgk08EaYJQ')
+        await homePage.openMenu('profile')
+        expect((await profilePage.ProfileName('EBAC Cliente')).isDisplayed()).toBeTruthy()
+    })
+})
+
