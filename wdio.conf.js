2abf45c89843a38665cf3a99a406e4ecda0f7907
Léo Kyrillos <leokyrillos@gmail.com>
2024-03-19 20:51:09 -0400

    inclusão de relatorios e screenshots

--- wdio.conf.js
+++ wdio.conf.js
@@ -20,10 +20,22 @@ export const config = {
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
     framework: 'mocha',
-    reporters: ['spec'],
+    reporters: ['spec',
+        ['allure', {
+            outputDir: 'allure-results',
+            disableWebdriverStepsReporting: true,
+            disableWebdriverScreenshotsReporting: false,
+        }]
+    ],
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000
     },
+    afterTest: async function(test, context, { error, result, duration, passed, retries }) {
+        //if(error)
+        {
+            await driver.takeScreenshot();
+        }
+    }
 
 }
